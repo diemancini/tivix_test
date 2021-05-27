e943cfa92d63dae2f5f48cf0e3b23cbf010f33ed
diemancini <diemancini@gmail.com>
2021-05-27 00:38:22 -0300

    Creating db models for Students and Teachers

--- app/teacher_student/crud/models.py
+++ app/teacher_student/crud/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Teachers(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Stars(models.Model):
+    teachers = models.ForeignKey('Teachers', on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        ordering = ['teachers']
+
+    def __str__(self):
+        return self.teachers
+
+
+class Students(models.Model):
+    name = models.CharField(max_length=50)
+    # stars = models.ForeignKey('Teachers',
+    #                           on_delete=models.CASCADE,
+    #                           null=True,
+    #                           related_name="teachers",
+    #                           related_query_name="teacher")
+    stars = models.ManyToManyField(Stars, null=True)
+    teachers = models.ManyToManyField(Teachers, null=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
--- app/teacher_student/crud/views.py
+++ app/teacher_student/crud/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from .models import Students
 
 @csrf_exempt
 def list_teachers(request):
 
     try:
-        if request.method == "POST":
-            return render(request, 'list_teachers.html')
+        json_request = {'teachers_array': [
+            {"name": "Diego Martin Mancini"},
+            {"name": "Luciana Agnoleto"},
+        ]}
+        if request.method == "GET":
+            return render(request, 'list_teachers.html', json_request)
 
         else:
             return render(request, 'list_teachers.html')
@@ -66,8 +71,20 @@ def delete_teachers(request):
 def list_students(request):
 
     try:
+        students_list = Students.objects.all()
+        for student in students_list:
+            print("---------------------")
+            print(student.name)
+            for teacher in student.teachers.all():
+                print("teacher: ", teacher.name)
+            for star in student.stars.all():
+                print("star: ", star.teachers.name)
+        json_request = {'students_list': [
+            {"name": "Diego Martin Mancini"},
+            {"name": "Luciana Agnoleto"},
+        ]}
         if request.method == "GET":
-            return render(request, 'list_students.html')
+            return render(request, 'list_students.html', json_request)
 
         else:
             return render(request, 'list_students.html')
